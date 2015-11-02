6e5b7594118a0c078c1796158d317cb34af07f8a
Rutul Dave <rutul98@yahoo.com>
2015-11-01 23:33:00 -0800

    Fix error in ajax after create and render of resolution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @resolutions = @user.resolutions
+    @all_resolutions = Resolution.all
   end
 
   def update
