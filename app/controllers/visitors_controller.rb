3681ef43535c8793dfc0118e25481012ea928c4e
Rutul Dave <rutul98@yahoo.com>
2015-11-02 20:06:20 -0800

    Better looking homepage (inspired by littlelogs.co)

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
   def index
     if current_user
       redirect_to user_path(current_user.id)
+    else
+      @events = Event.all.order("created_at DESC").limit(15)
     end
   end
 end
