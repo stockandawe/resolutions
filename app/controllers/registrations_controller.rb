1a33f51f2bf6e64e20d7065e63e436ad2dc7920b
Rutul Dave <rutul98@yahoo.com>
2015-11-02 19:18:34 -0800

    Customize Devise
    - Not require a current_password for user#edit.
    - Don’t require a password_confirmation on signup.
    - Allow User.name to be entered and saved.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  devise_for :users
-  resources :users, only: [:show]
 
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :users, only: [:show]
   resources :resolutions
 end
